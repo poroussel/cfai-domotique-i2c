f34a96ee0cfa0b3139554b391ca7aa2cef550017
Philippe Roussel <philippe.roussel@octets.fr>
2017-02-06 10:29:40 +0000

    Passage du serveur en classe

--- server.py
+++ server.py
@@ -4,6 +4,8 @@ import sys
 import select
 import argparse
 import logging
+import time
+
 from config import CONFIG
 
 FORMAT = "%(asctime)s %(levelname)s:%(name)s:%(lineno)d : %(message)s"
@@ -17,39 +19,50 @@ from creation_BDD import create_bdd
 from Ypareo import Ypareo
 from i2c import BusI2C
 
-def serve():
-    bus = BusI2C()
-    ypareo = Ypareo()
+class Server(object):
+    def __init__(self):
+        self.bus = BusI2C()
+        self.ypareo = Ypareo()
 
-    if not ypareo.connexion():
-        logging.error('Erreur connexion ypareo')
+        if not self.ypareo.connexion():
+            logging.error('Erreur connexion ypareo')
 
-    loop = True    
-    inl = [sys.stdin]
+        # Liste des capteurs que l'on doit lire
+        self.inputs = [name for name, conf in CONFIG['hardware'].iteritems() if conf['action'] == 'read']
 
-    logging.info('Lancement du serveur...')
-    while loop:
-        try:
-            readable, _, _ = select.select(inl, [], [], CONFIG['temporisation'])
+    def handle_input(self):
+        for cpt in self.inputs:
+            val = self.bus.read(cpt)
+            print 'Lecture {} : {}'.format(cpt, val)
+            time.sleep(0.1)
+        
+    def run(self):
+        logging.info('Lancement du serveur...')
+        loop = True
+        inl = [sys.stdin]
+        while loop:
+            try:
+                readable, _, _ = select.select(inl, [], [], CONFIG['temporisation'])
 
-            # Sortie par timeout
-            if not readable:
-                try:
-                    planning = ypareo.interroPlanning()
-                except:
-                    logging.exception('Lecture planning')
-            else:
-                for h in readable:
-                    if h == sys.stdin:
-                        line = sys.stdin.readline()
+                # Sortie par timeout
+                if not readable:
+                    try:
+                        planning = self.ypareo.interroPlanning()
+                    except:
+                        logging.exception('Lecture planning')
+                    self.handle_input()
+                else:
+                    for h in readable:
+                        if h == sys.stdin:
+                            line = sys.stdin.readline()
                     
-        except KeyboardInterrupt:
-            logging.info('Interruption clavier')
-            loop = False
+            except KeyboardInterrupt:
+                logging.info('Interruption clavier')
+                loop = False
 
-    ypareo.deconnexion()
-    logging.info('Fermeture du serveur')
-
+        self.ypareo.deconnexion()
+        logging.info('Fermeture du serveur')
+        
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -68,7 +81,7 @@ if __name__ == "__main__":
         create_bdd()
         print '...terminé'
     elif env.run:
-        serve()
+        Server().run()
     elif env.beep:
         BusI2C().cmd('beep')
     elif env.execute:
