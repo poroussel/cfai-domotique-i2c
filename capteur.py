2ba49446dc75c6484f910477f5aff5f96c03ab48
Philippe Roussel <p.o.roussel@free.fr>
2019-02-15 13:14:26 +0100

    RFID : Le capteur rfid/modbus retourne le tag vu s'il est nouveau, None sinon

--- capteur.py
+++ capteur.py
@@ -4,6 +4,9 @@ import socket
 import struct
 import time
 
+from logging import getLogger
+logger = getLogger(__name__)
+
 class Capteur(object):
     def __init__(self, name, conf):
         self.name = name
@@ -19,34 +22,33 @@ class CapteurI2C(Capteur):
 
 
 class CapteurModBUS(Capteur):
+    last_seen = None
+    
     def read(self, srv):
-
         SEND_READ = struct.pack("BBBBBBBBBBBB", 0, 0, 0, 0, 0, 6, self.conf["slave"], 0x3, 0x80, 0x00, 0x0, 0xA,);
 
         connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
         try:
             connection.connect((self.conf["tcp"], self.conf["port"]))
         except:
-            return -1
-
+            return None
 
         connection.send(SEND_READ)
-
         RCV = connection.recv(64)
-            
         RCV = struct.unpack('B' * 29, RCV)
+        connection.close()
     
         P_BADGE = RCV[9]
         ID_BADGE = RCV[13]
-    
-        ACCESS = 2
-        if P_BADGE and ID_BADGE == 164:
-            print "Badge Accepted"
-            ACCESS = 1
-        elif P_BADGE:
-            print "Badge Refused"
-            ACCESS = 0
-        connection.close()
 
-        return ACCESS
+        # Un tag RFID a été lu
+        if P_BADGE:
+            if ID_BADGE == self.last_seen:
+                logger.info("Tag deja vu")
+                return None
+            self.last_seen = ID_BADGE
+            logger.info('Nouveau tag : {}'.format(ID_BADGE))
+            return ID_BADGE
+        
+        return None
+    
